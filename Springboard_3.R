b924bd45cb23f245df6677f94471fac9c1aabbbd
ababen <alexbaben@gmail.com>
2016-08-23 02:36:08 -0400

    Cleaned up the code a bit

--- Springboard_3.R
+++ Springboard_3.R
@@ -4,9 +4,13 @@ install.packages("devtools")
 library("tidyr")
 library("dplyr")
 library(devtools)
+
 refine_original <- read.csv("/users/ababen/Springboard/refine_original.csv")
 View(refine_original)
 refine1 <- refine_original
+
+# I have tried the following two lines of code one-by-one.
+
 refine2 <- unite(refine1,full_address,c("address","city"), sep = ",",remove = TRUE)
 refine2 <- unite(refine1,full_address,c("address":"country"), sep = ",",remove = TRUE)
 
@@ -17,16 +21,15 @@ refine3 # I don not understand why gsub placed the
 
 # This is the example I am working off of:
 
+
 #Case insensitive replace:
 #  > gsub("tut","ot",x,ignore.case=T))
 #[1] "R otorial"
 
 # I am going to attemp the same with replace()
 
-if (refine1 != null) {
-  refine1 <- refine_original
-}
 replace(refine1, c("ak zo","akzo"), "akzo")
+View(refine1)
 
 # Cheated here and used Excel to get the below list. Wonder if there is any easy way of doing this with R.
 # c("ak zo", "akz0", "AKZO", "Akzo", "akzo")
